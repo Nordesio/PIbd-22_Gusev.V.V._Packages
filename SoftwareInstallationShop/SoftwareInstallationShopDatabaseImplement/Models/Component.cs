d6277f555b8ebce96dbdadc6c06555e9fba9d49b
vladg <vladg@DESKTOP-CB3DP3M>
2022-03-16 19:00:37 +0400

    commit

---
+++ SoftwareInstallationShop/SoftwareInstallationShopDatabaseImplement/Models/Component.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftwareInstallationShopDatabaseImplement.Models
+{
+    public class Component
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ComponentName { get; set; }
+
+      //  [ForeignKey("ComponentId")]
+        //public virtual List<PackageComponent> PackageComponents { get; set; }
+    }
+}
---
+++ SoftwareInstallationShop/SoftwareInstallationShopDatabaseImplement/SoftwareInstallationShopDatabase.cs
@@ -0,0 +1,23 @@
+﻿using SoftwareInstallationShopDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftwareInstallationShopDatabaseImplement
+{
+    public class SoftwareInstallationShopDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=HOME\SQLEXPRESS;Initial
+Catalog=AbstractShopDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Component> Components { set; get; }
+        public virtual DbSet<Package> Packages { set; get; }
+        public virtual DbSet<ProductComponent> ProductComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+
+    }
+}
